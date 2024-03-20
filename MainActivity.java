c38185acf8a3e7972525c080565a22af0728a66e
DanielNowacki2005 <144656552+DanielNowacki2005@users.noreply.github.com>
2024-03-20 21:20:14 +0100

    co zrobiłem daniel
    dodaje co udało mi się zrobić.

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.aplikacja_sklep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //zmienne które są by użyć listenera
+    ImageView imageView1 = (ImageView) findViewById(R.id.imageView3);
+    ImageView imageView2 = (ImageView) findViewById(R.id.imageView);
+    ImageView imageView3 = (ImageView) findViewById(R.id.imageView2);
+    ImageView imageView4 = (ImageView) findViewById(R.id.imageView6);
+    ImageView imageView5 = (ImageView) findViewById(R.id.imageView7);
+    ImageView imageView6 = (ImageView) findViewById(R.id.imageView8);
+    ImageView imageView3 = (ImageView) findViewById(R.id.imageView9);
+    //listener wywołuje funcje która tworzy intent gdzie MainActivityAfter jest to strona docelowa
+    imageView1.setOnClickListener(new OnClickListener() {onClickChangeView(MainActivity.this)});
+    public void onClickChangeView(View v,MainActivityAfter) {
+        Intent intent = new Intent(this,MainActivityAfter.class);
+        startActivity(intent);
+        CustomIntent.customType(MainActivity.this , "right-to-left");}
+
+}
